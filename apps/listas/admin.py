840579ee7a6968b545338798db7017a1e0b32513
jairoerazobotero <jairoerazo@hotmail.com>
2019-10-04 22:32:46 +0200

    Admin filter

--- apps/listas/admin.py
+++ apps/listas/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from apps.listas.models import Lists, Book, Gender
 
-# Register your models here.
-admin.site.register(Lists)
-admin.site.register(Book)
-admin.site.register(Gender)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    search_fields = ['name']
+    list_per_page = 20  # No of records per page
+
+@admin.register(Lists)
+class ListsAdmin(admin.ModelAdmin):
+    list_per_page = 20  # No of records per page
+
+@admin.register(Gender)
+class GenderAdmin(admin.ModelAdmin):
+    list_per_page = 20  # No of records per page
